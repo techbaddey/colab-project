4971faeddd6319381e1e393089f43d1ce19e5734
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2023-05-10 17:57:40 +0100

    project page init

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import Mydomot from '../Assests/MyDomot.png'
+import metabnb from '../Assests/MetaBnb.png'
+import portfolio from '../Assests/portfolio.png'
+import puzzle from '../Assests/puzzle.png'
+
 
 export default function Project() {
   return (
     <>
     <Header />
     <div>
+    <img className="image" src={Mydomot} alt={" "} />
+    <img className="image" src={metabnb} alt={" "} />
+    <img className="image" src={puzzle} alt={" "} />
+    <img className="image" src={portfolio} alt={" "} />
         Project
     </div>
     <Footer />
