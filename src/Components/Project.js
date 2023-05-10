40820a9b6f15819007625b59080ccfc4ae9667c4
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2023-05-10 19:00:41 +0100

    fixed a bug

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -14,19 +14,33 @@ export default function Project() {
     <div className='project-container'>
       <h3>My Recent Projects</h3>
 
+<div className='project-one'>
       <div>
     <img className="image" src={Mydomot} alt={" "} />
+    <p>React JS</p> <p>CSS</p> <p>Vercel</p>
+    <button>Live Demo</button>  <button>Source Code</button>
     </div>
     <div>
     <img className="image" src={metabnb} alt={" "} />
+    <p>React JS</p> <p>Sass</p> <p>Vercel</p>
+    <button>Live Demo</button>  <button>Source Code</button>
+    </div>
     </div>
 
+
+<div className='project-two'>
     <div>
     <img className="image" src={puzzle} alt={" "} />
+    <p>React JS</p> <p>CSS</p> <p>Vercel</p>
+    <button>Live Demo</button>  <button>Source Code</button>
     </div>
     <div>
     <img className="image" src={portfolio} alt={" "} />
+    <p>Vanilla JS</p> <p>Netlify</p> 
+    <button>Live Demo</button>  <button>Source Code</button>
       </div>
+      </div>
+
 
         <button> <a href='https://github.com/teqbaddie'> More in Github </a></button>
     </div>
